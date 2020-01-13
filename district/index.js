24458b54ba9e3cba2913045e47e5ff9b6342f3bb
ruanzhijun <ruanzhijun@ruanzhijun.cn>
2020-01-13 15:42:10 +0800

    更新全国行政区域编码

--- district/index.js
+++ district/index.js
@@ -23,7 +23,7 @@ async function commander(cmd) {
 
 async function main() {
   rimraf.sync(districtFile);
-  const result = await request('http://www.mca.gov.cn/article/sj/xzqh/2019/201908/201908271607.html');
+  const result = await request('http://www.mca.gov.cn/article/sj/xzqh/2019/2019/201912251506.html');
   const $ = cherio.load(result);
   const tr = $('table tr');
 
